6302340ad3d7ad31590d59d17b0c25a34be78682
MahmoudKrima <modykrima2000@gmail.com>
2025-09-07 23:50:22 +0300

    fix testominals

--- resources/views/dashboard/pages/testimonial/edit.blade.php
+++ resources/views/dashboard/pages/testimonial/edit.blade.php
@@ -21,40 +21,22 @@
                                     <div class="form-group">
                                         <div class="row">
                                             <div class="col-6 mb-3">
-                                                <label for="name_ar"
-                                                    class="text-dark">{{ __('admin.name_ar') }}</label>
-                                                <input type="text" name="name_ar" id="name_ar" class="form-control"
-                                                    value="{{ old('name_ar', $testimonial->getTranslation('name', 'ar')) }}">
+                                                <label for="name"
+                                                    class="text-dark">{{ __('admin.name') }}</label>
+                                                <input type="text" name="name" id="name" class="form-control"
+                                                    value="{{ old('name', $testimonial->getTranslation('name', 'ar')) }}">
                                             </div>
                                             <div class="col-6 mb-3">
-                                                <label for="name_en"
-                                                    class="text-dark">{{ __('admin.name_en') }}</label>
-                                                <input type="text" name="name_en" id="name_en" class="form-control"
-                                                    value="{{ old('name_en', $testimonial->getTranslation('name', 'en')) }}">
-                                            </div>
-                                            <div class="col-6 mb-3">
-                                                <label for="job_title_ar"
-                                                    class="text-dark">{{ __('admin.job_title_ar') }}</label>
-                                                <input type="text" name="job_title_ar" id="job_title_ar" class="form-control"
-                                                    value="{{ old('job_title_ar', $testimonial->getTranslation('job_title', 'ar')) }}">
-                                            </div>
-                                            <div class="col-6 mb-3">
-                                                <label for="job_title_en"
-                                                    class="text-dark">{{ __('admin.job_title_en') }}</label>
-                                                <input type="text" name="job_title_en" id="job_title_en" class="form-control"
-                                                    value="{{ old('job_title_en', $testimonial->getTranslation('job_title', 'en')) }}">
+                                                <label for="job_title"
+                                                    class="text-dark">{{ __('admin.job_title') }}</label>
+                                                <input type="text" name="job_title" id="job_title" class="form-control"
+                                                    value="{{ old('job_title', $testimonial->getTranslation('job_title', 'ar')) }}">
                                             </div>
                                             <div class="col-12 mb-3">
-                                                <label for="review_ar"
-                                                    class="text-dark">{{ __('admin.review_ar') }}</label>
-                                                <textarea name="review_ar" id="review_ar" class="form-control"
-                                                    rows="3">{{ old('review_ar', $testimonial->getTranslation('review', 'ar')) }}</textarea>
-                                            </div>
-                                            <div class="col-12 mb-3">
-                                                <label for="review_en"
-                                                    class="text-dark">{{ __('admin.review_en') }}</label>
-                                                <textarea name="review_en" id="review_en" class="form-control"
-                                                    rows="3">{{ old('review_en', $testimonial->getTranslation('review', 'en')) }}</textarea>
+                                                <label for="review"
+                                                    class="text-dark">{{ __('admin.review') }}</label>
+                                                <textarea name="review" id="review" class="form-control"
+                                                    rows="3">{{ old('review', $testimonial->getTranslation('review', 'ar')) }}</textarea>
                                             </div>
                                             <div class="col-6 mb-3">
                                                 <label for="rate"
