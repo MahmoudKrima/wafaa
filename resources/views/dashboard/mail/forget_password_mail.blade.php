1eba8946fcf9e5f2d79d8b3d03aae5c45ff5bf80
ahmedredasa3eed <ahmedredasa3eed@gmail.com>
2025-09-09 04:23:48 +0300

    mail fixes11

--- resources/views/dashboard/mail/forget_password_mail.blade.php
+++ resources/views/dashboard/mail/forget_password_mail.blade.php
@@ -12,7 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width">
-    <title>{{ __('admin.new_reply') }}</title>
+    <title>{{ __('admin.forget_password_request') }}</title>
     @if (App::getLocale() === 'ar')
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
